7b507c8f715b5743f67580cbd5f8835f6feb1f53
Robert Kesterson <rsk23@uw.edu>
2019-01-27 20:13:24 -0800

    checking in stubs for list_lab

---
+++ students/robert_kesterson/session03/list_lab.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------- #
+# Title: Lesson 03 Exercise: List Lab
+# Desc: This is a lab where we will learn about list manipulation
+# Change log: (who, when, what)
+# RKesterson, 2019-01-27, Created file
+# RKestesron, 2019-01-27, Stubbed out methods / parts
+# RKesterson, 2019-01-XX, Completed series one
+# RKesterson, 2019-01-XX, Completed series two
+# RKesterson, 2019-01-XX, Completed series three
+# RKesterson, 2019-01-XX, Completed series four
+# ---------------------------------------------- #
+
+# Series one
+
+# Series two
+
+# Series three
+
+# Series four
