0ce7f2b55a15a632f27743048d6fe37552b83022
Robert Kesterson <rsk23@uw.edu>
2019-02-03 18:35:20 -0800

    done with dictionaries

--- students/robert_kesterson/session04/dictlab.py
+++ students/robert_kesterson/session04/dictlab.py
@@ -6,12 +6,32 @@
 # Change log: (who, when, what)
 # RKesterson, 2019-01-30, Created file
 # RKestesron, 2019-01-30, Stubbed out methods / parts
+# RKestesron, 2019-02-03, Completed dictionaries
+
 # Note - Commit to FebUpdates branch when creating pull request this week
 # ---------------------------------------------- #
 
 # Dictionaries
+dictOne = {
+    'name': 'Chris',
+    'city': 'Seattle',
+    'cake': 'Chocolate'
+}
+for i in dictOne.keys():
+    print("Key: {:s} | Value: {:s}".format(i, dictOne[i]))
+
+del dictOne['cake']
+for i in dictOne.keys():
+    print("{:s}: {:s}".format(i, dictOne[i]))
+
+dictOne['fruit'] = 'Mango'
+print(dictOne.keys())
+print(dictOne.values())
+print('cake' in dictOne)
+print('Mango' in dictOne.values())
 
 # Dictionaries 2
+
 
 # Sets
 
