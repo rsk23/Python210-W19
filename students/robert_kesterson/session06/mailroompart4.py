57f874d8b7c1b06488ebbbb226750774be254212
Robert Kesterson <rsk23@uw.edu>
2019-02-19 16:31:37 -0800

    Adding in unit tests

---
+++ students/robert_kesterson/session06/mailroompart4.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------- #
+# Title: Lesson 06 Assignment: Mailroom, Part 4
+# Desc: Add a full suite of unit tests.
+# Change log: (who, when, what)
+# RKesterson, 2019-02-13, Created file
+# RKestesron, 2019-02-16, Copied over content from mailroompart3
+# RKestesron, 2019-02-16, Refactored code from mailroom part 3 to better support unit tests
+# ---------------------------------------------- #
+
+donationdict = {}
+
+def load_donation_dict():
+    global donationdict
+    donationdict = {
+        'Winston Churchill': [20, 50],
+        'Hunter S. Thompson': [50, 500, 5000],
+        'Richard Garfield': [300, 400],
+        'Simon Illyan': [100],
+        'Theodore Roosevelt': [50, 100]
+    }
+
+def send_thank_you():
+    global donationdict
+    tyresponse = input("Please enter a full name. list provides all names")
+    while True:
+        if tyresponse == "list":
+            for i in donationdict.keys():
+                print("{:s}".format(i))
+            tyresponse = input("Please enter a full name. List provides all names")
+        else:
+            donation = input("Enter a donation amount as a whole dollar integer value.")
+            try:
+                donation = int(donation)
+            except ValueError:
+                print("Input must be an integer. Try again")
+                break
+            if tyresponse in donationdict.keys():
+                donvalue = donationdict[tyresponse]
+                donvalue.append(donation)
+                donationdict[tyresponse] = donvalue
+            else:
+                donvalue = []
+                donvalue.append(donation)
+                donationdict[tyresponse] = donvalue
+            print("Thank you {:s} for your generous donation of {:d} dollars".format(tyresponse, donation))
+            break
+
+def sort_key(donor):
+    return donor[1]
+
+def create_a_report():
+    donationlist = []
+    for i in donationdict.keys():
+        donationcount = 0
+        donationtotal = 0
+        donationvalue = donationdict[i]
+        for j in donationvalue:
+            donationtotal += int(j)
+            donationcount += 1
+        averagedonation = donationtotal / donationcount
+        tupleone = (i, donationtotal, donationcount, averagedonation)
+        donationlist.append(tupleone)
+    sorteddonorlist = sorted(donationlist, key=sort_key)
+    sorteddonorlist.reverse()
+    print("Donor Name | Total Given | Num Gifts | Average Gift")
+    print("---------------------------------------------------")
+    for k in sorteddonorlist:
+        print("{:s} ${:d} {:d} ${:.2f}".format(k[0], k[1], k[2], k[3]))
+
+def break_function():
+    exit()
+
+def send_letters_to_all_donors():
+    global donationdict
+    for i in donationdict.keys():
+        filename = "{:s}.txt".format(i)
+        outfile = open(filename, 'w')
+        donationtotal = 0
+        donationvalue = donationdict[i]
+        for j in donationvalue:
+            donationtotal += int(j)
+        messagestring = "Dear {:s}, \r\n\n    Thank you for your very kind donation of ${:d}.\r\n\n    It will be put" \
+                        " to very good use.\r\n\n            Sincerely,\r\n                -The Team".format(i, donationtotal)
+        outfile.write(messagestring)
+        outfile.close()
+
+def load_selection_list():
+    switch_function_dict = {
+        '1 - Send a Thank You to a single donor.': send_thank_you,
+        '2 - Create a Report.': create_a_report,
+        '3 - Send letters to all donors.': send_letters_to_all_donors,
+        '4 - Quit': break_function
+    }
+    return switch_function_dict
+
+if __name__ == '__main__':
+    switch_function_dict = load_selection_list()
+    load_donation_dict()
+
+    while True:
+        print("Chose an action: \r\n")
+        for i in switch_function_dict.keys():
+            print("{:s}".format(i))
+        response = input("")
+        if response not in switch_function_dict.keys():
+            print("You must chose an action from the approved list. Actions are: \r\n")
+            for i in switch_function_dict.keys():
+                print("{:s}".format(i))
+            response = input("")
+        else:
+            switch_function_dict.get(response)()
---
+++ students/robert_kesterson/session06/unit_tests_mailroompart4.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# -------------------------------------------- #
+# Title: Lesson 06 Assignment: Mailroom, Part 4
+# Desc: Add a full suite of unit tests.
+# Change log: (who, when, what)
+# RKesterson, 2019-02-13, Created file
+# RKestesron, 2019-02-16, Began defining unit tests
+# RKestesron, 2019-02-16, Refined unit tests
+# ---------------------------------------------- #
